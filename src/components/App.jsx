f9a6295aa279c2cf450d38d5e16dc4f3cd3e8d81
OlenaKovalenko <elenkow17@gmail.com>
2023-11-01 20:56:48 +0200

    Create App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react";
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+
+  render() {
+    return (
+      <section>
+        <h1>Please leave feedback</h1>
+        <div>
+          <button>Good</button>
+          <button>Neutral</button>
+          <button>Bad</button>
+        </div>
+        <div>
+          <h2>Statistics</h2>
+          <p>Good: </p>
+          <p>Neutral: </p>
+          <p>Bad: </p>
+        </div>
+      </section>
+    )
+  }
+}
